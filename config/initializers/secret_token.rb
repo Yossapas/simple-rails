088eea058a046649a62821028a3f93b8e534c4df
Yossapas <lucifer_ap@hotmail.com>
2010-09-29 01:49:49 -0700

    add Post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleRails::Application.config.secret_token = '855c51d6b3a81a5e5b4a101b289269e35b8f7df1edca47a11b12a698f644e12ae022d03ef7c053401c88556d7d0ae9df2c1bc41ec2b0da45a3425168537ff501'
