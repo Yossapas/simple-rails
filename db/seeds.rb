8ca9acbe9ee1081185b6ff13d1b414372a541cd4
Yossapas <lucifer_ap@hotmail.com>
2010-09-30 01:25:11 -0700

    Create Product in seeds.rb (2)

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,59 @@ Product.create(
 :name => "Scosche metalliKASE Case and Mirror Screen Protector for iPod touch 4G (Chrome)", 
 :description => "Low Profile Polycarbonate Case for iPod touch Polycarbonate shell provides protection against knocks and nicks 3 Pack of Flexible Rubber Cases for new iPod touch Rubber edge cases protect phone while adding little to no bulk Lay on the table design protects screen from scratches Unique glossy material provides enhanced grip and superior protection Open design provides complete access to all connections and controls", 
 :price => 24.99)
+
+Product.create(
+:name => "Apple iPod touch 8 GB (4th Generation) NEWEST MODEL", 
+:description => "See friends while you talk to them with FaceTime. Shoot, edit, and share stunning HD video. Play games against friends, or unknown foes, with the new Game Center. And do it all on the Retina display--the highest-resolution screen on any mobile device. It makes graphics and text look even more amazing. The new iPod touch. It's state-of-the-art fun.", 
+:price => 223.99)
+
+Product.create(
+:name => "Kindle Wireless Reading Device, Wi-Fi, 6 Display, Graphite - Latest Generation", 
+:description => "Kindle is our #1 bestselling item for two years running. Its also the most-wished-for, most-gifted, and has the most 5-star reviews of any product on Amazon. Now it's even better.", 
+:price => 139.00)
+
+Product.create(
+:name => "Apple iPod touch 8 GB (2nd Generation--with iPhone OS 3.1 Software Installed) [OLD MODEL]", 
+:description => "iPod touch is a great iPod, a great pocket computer, and a great portable game player. Listen to a mix of songs automatically put together by the new Genius Mixes feature. Watch a movie. Surf the web. Download countless apps. (Click here to jump down the page and learn all about apps.) View rich HTML email. Find your location and get directions with Google Maps. Discover games and apps you're sure to love with new Genius recommendations for apps. And since iPod touch now comes with the latest 3.1 software already installed, you can start using fun, convenient features right out of the box. View an interactive demo on the iPod touch (opens a new window).", 
+:price => 169.97)
+
+Product.create(
+:name => "Scosche glosSEE 3-Pack TPU Skins and Screen Protector for iPod touch 4G (Dark)", 
+:description => "3 Pack of Flexible Rubber Cases for new iPod touch Rubber edge cases protect phone while adding little to no bulk", 
+:price => 28.81)
+
+Product.create(
+:name => "Swiss+Tech UKCSB-1 Utili-Key 6-in-1 Key Ring Tool", 
+:description => "Swiss+Tech Utili-Key 6-in-1 Key Ring Tool is a light weight self locking tool that securely attaches to your key ring. It features a flat screwdriver, bottle opener, phillips screwdriver, serrated and straight knife blades as well as a micro eyeglass screwdriver. It is compact and convenient to use.", 
+:price => 8.95)
+
+Product.create(
+:name => "LifeHammer Orange Original Emergency Hammer", 
+:description => "LifeHammer not only is the next step in the evolution of vehicle safety, but can also be held in the palm of the driver's hand. Consisting of a double-tipped hardened steel hammer, and a safe seat belt cutting blade, it is designed as an aid drivers and passengers who, in the event of an emergency, need to quickly escape from their vehicle. Whether the emergency is due to submersion in water, a fire or a related event, this lifesaving device is easily stored out of the way, yet within range of the driver.", 
+:price => 14.95)
+
+Product.create(
+:name => "AAA 73 Piece Adventurer Road Assistance Kit", 
+:description => "You never know where the road may lead or what may happen, so don't be caught unprepared. LifeLine First Aid--a leader in the field of first-aid and disaster readiness products--has teamed up with AAA to bring you a line of high quality Road Assistance Kits. Featuring both first-aid and automotive emergency items, these practical all-in-one kits can help protect you in the event of an automobile breakdown or personal injury.", 
+:price => 45.11)
+
+Product.create(
+:name => "Yo Gabba Gabba! Play Along Plush - Foofa", 
+:description => "Tumble her around, hold her upside down, Play Along Foofa knows exactly what you're doing and joins in the fun!  She is full of silly words and funny sounds.  So dance, tumble, jump, turn upside down, sleep, cuddle and jiggle with the silliest monster around! ", 
+:price => 28.99)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
