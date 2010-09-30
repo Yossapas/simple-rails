69279bf4a37751aee1220e0c24220a562d92daed
Yossapas <lucifer_ap@hotmail.com>
2010-09-30 01:06:19 -0700

    Create Product in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+Product.create(
+:name => "Apple iPod nano 16 GB Orange (6th Generation) NEWEST MODEL", 
+:description => "Redesigned for music and Multi-Touch, iPod nano is smaller and lighter than ever. You can clip it to your sleeve, jacket, or running shorts and keep your favorite songs at your fingertips--along with the Genius feature, a built-in FM radio, pedometer, and more. And the anodized aluminum finish in six bright colors makes this new design even more brilliant.", 
+:price => 179.00)
+
+Product.create(
+:name => "Scosche metalliKASE Case and Mirror Screen Protector for iPod touch 4G (Chrome)", 
+:description => "Low Profile Polycarbonate Case for iPod touch Polycarbonate shell provides protection against knocks and nicks 3 Pack of Flexible Rubber Cases for new iPod touch Rubber edge cases protect phone while adding little to no bulk Lay on the table design protects screen from scratches Unique glossy material provides enhanced grip and superior protection Open design provides complete access to all connections and controls", 
+:price => 24.99)
